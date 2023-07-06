7513ea3bd2a46ded5e4c772749cccc87f09200a0
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-06 23:24:04 +0600

    All tasks have been done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String result = "";
+        while (number > 0) {
+            result += number % 10;
+            number /= 10;
+        }
+        System.out.println(result);
     }
 }
